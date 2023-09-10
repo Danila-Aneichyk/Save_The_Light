8a1c4d03b134032b4083c1a2bc2ca38fa4115d1a
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-09-10 18:05:19 +0300

    1. Create a base of the level. 2. Add player movement script.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Movement values")]
+        private float _speed = 7.0f;
+        private Vector3 direction = -Vector3.left;
+
+        [Header("Jump values")]
+        public float jumpForce = 10f;
+        private bool isGrounded = true;
+
+        void Update()
+        {
+            Move();
+            Jump();
+        }
+
+        private void Move()
+        {
+            Vector3 newPosition = transform.position + direction.normalized * (_speed * Time.deltaTime);
+            transform.position = newPosition;
+        }
+
+        private void Jump()
+        {
+            if ((Input.GetButtonDown("Jump") || Input.touchCount > 0) && isGrounded)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(0f, jumpForce, 0f);
+                isGrounded = false;
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+    }
+}
