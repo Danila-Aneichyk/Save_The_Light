3b2c5b2c3a94773722717f93db2decee93ecea2d
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-09-22 22:23:08 +0300

    1. Added event that invokes when level end. 2. Remove some methods from Win script.

--- Assets/Scripts/Win/Win.cs
+++ Assets/Scripts/Win/Win.cs
@@ -1,43 +1,30 @@
 ﻿using System;
 using LevelControl;
 using Names;
+using Player;
 using UnityEngine;
 
 namespace Win
 {
     public class Win : MonoBehaviour
     {
+        private PlayerMovement _playerMovement;
+
         private bool _isWin = false;
-
-        private void Awake()
-        {
-            LevelStateMachine.Instance.OnGameStateChanged += LevelStateMachine_OnGameStateChanged;
-        }
-
-        private void LevelStateMachine_OnGameStateChanged(object sender, LevelStates e)
-        {
-            if (e == LevelStates.LevelEnd)
-            {
-                StopGameplay();
-                ShowWinUI();
-            }
-        }
-
-        private void StopGameplay()
-        {
-        }
-
-        private void ShowWinUI()
-        {
-        }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag(Tags.Player))
             {
+                other.gameObject.GetComponent<PlayerMovement>();
                 _isWin = true;
                 LevelStateMachine.Instance.state = LevelStates.LevelEnd;
             }
+        }
+
+        private void StopGameplay()
+        {
+            _playerMovement.enabled = false;
         }
     }
 }
