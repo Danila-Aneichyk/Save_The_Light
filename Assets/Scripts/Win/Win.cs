47052b7036b019a3b7fd6e90cefccab5ebf06a32
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-09-22 12:26:25 +0300

    1. Started to implement logic of win.

---
+++ Assets/Scripts/Win/Win.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using LevelControl;
+using Names;
+using UnityEngine;
+
+namespace Win
+{
+    public class Win : MonoBehaviour
+    {
+        private bool _isWin = false;
+
+        private void Awake()
+        {
+            LevelStateMachine.Instance.OnGameStateChanged += LevelStateMachine_OnGameStateChanged;
+        }
+
+        private void LevelStateMachine_OnGameStateChanged(object sender, LevelStates e)
+        {
+            if (e == LevelStates.LevelEnd)
+            {
+                StopGameplay();
+                ShowWinUI();
+            }
+        }
+
+        private void StopGameplay()
+        {
+        }
+
+        private void ShowWinUI()
+        {
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag(Tags.Player))
+            {
+                _isWin = true;
+                LevelStateMachine.Instance.state = LevelStates.LevelEnd;
+            }
+        }
+    }
+}
